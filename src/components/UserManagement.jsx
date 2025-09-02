c11c477170bef4047e9655782c28230bd1b99a76
kayesmahmud000 <mdkayesmahmud3@gmail.com>
2025-09-03 03:05:29 +0600

    added the user data table

--- src/components/UserManagement.jsx
+++ src/components/UserManagement.jsx
@@ -1,4 +1,5 @@
 import SearchInputWithButton from "./SearchInputWithButton";
+import UserDataTable from "./UserDataTable";
 
 
 const UserManagement = () => {
@@ -6,8 +7,11 @@ const UserManagement = () => {
         <div className="bg-white w-full rounded-xl shadow-md px-5 py-5 min-h-[100px]">
             <h2 className="text-2xl font-semibold">User Management </h2>
 
-            <div className="w-full">
+            <div className="w-full my-5">
                 <SearchInputWithButton/>
+            </div>
+            <div>
+                <UserDataTable/>
             </div>
         </div>
     );
